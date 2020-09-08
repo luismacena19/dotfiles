a3ea57706c53f2e23008d946f113fd90a30d9e9a
Luis Macena <luis.macena@outlook.com>
2020-09-08 03:34:34 -0300

    Add more configfiles

---
+++ .dwm/autostart.sh
@@ -0,0 +1,13 @@
+./dwm_status &
+nm-applet &
+blueman-applet & 
+setxkbmap -model abnt2 -layout br -variant abnt2
+picom &
+clipmenud &
+redshift-gtk &
+dunst &
+sleep 3 && feh --bg-scale /home/nullroot_/Documents/Pictures/Some_Concepts/1042815.jpg
+#sleep 4 && notify-send "OndeDrive atualizando" && onedrive --synchronize && notify-send "OneDrive atualizado"
+#sleep 10 && teams &
+#sleep 1 && tela & #This is a script to ajust my resolution on a external monitor when it is pluged
+# export _JAVA_AWT_WM_NONREPARENTING=1
