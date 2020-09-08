ddc92d4f6bc2ad5a81571e86c1c25735d2245bba
Luis Macena <luis.macena@outlook.com>
2020-09-08 03:48:03 -0300

    Update

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -6,8 +6,5 @@ picom &
 clipmenud &
 redshift-gtk &
 dunst &
-sleep 3 && feh --bg-scale /home/nullroot_/Documents/Pictures/Some_Concepts/1042815.jpg
-#sleep 4 && notify-send "OndeDrive atualizando" && onedrive --synchronize && notify-send "OneDrive atualizado"
-#sleep 10 && teams &
-#sleep 1 && tela & #This is a script to ajust my resolution on a external monitor when it is pluged
+sleep 3 && feh --bg-scale <path to some picture>
 # export _JAVA_AWT_WM_NONREPARENTING=1
