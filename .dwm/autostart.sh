7f1809223b604deeafc0071c653f40c895cced0e
Luis Macena <luis.macena@outlook.com>
2021-10-09 13:37:39 -0300

    Add script to choose browser and profile. Add the dwm-bottomstack-6.1 path, and the clipmenu call was also changed to match the dmenu style. Add dunstrc and redshift.conf(lat and long need to be change), also add gnome keyring to autostart.sh. Fix some problems with fade_in_wallpaper and mylock.

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -7,4 +7,5 @@ clipmenud &
 redshift-gtk &
 dunst &
 sleep 1 && tela & #This is a script to ajust my resolution on a external monitor when it is pluged
+gnome-keyring-daemon --start 1> /dev/null
 # echo "_JAVA_AWT_WM_NONREPARENTING=1" > /etc/profile.d/jre.sh
